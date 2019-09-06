5445f0a6b02791ec3c6f7e58d8b8d6a19cca497d
Logan <ljwhite993@gmail.com>
2019-09-06 15:41:39 -0600

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Logan White on 9/6/19.
+//  Copyright © 2019 Logan White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
