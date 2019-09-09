7663bac9d5674a7a3c915e44b22d978203b72b22
Logan <ljwhite993@gmail.com>
2019-09-09 12:23:28 -0600

    Convert to MVC design pattern

---
+++ Todo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Logan White on 9/9/19.
+//  Copyright © 2019 Logan White. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
